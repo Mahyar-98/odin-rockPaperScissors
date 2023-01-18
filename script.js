c3483a6bcf54901b3f3eb1ced9d58c51edb56080
Mahyar <mahyar.erfanian1998@gmail.com>
2023-01-18 16:38:23 -0500

    Add declareWinner function to JS file
    This function askes the user through the browser prompt to
    select rock, paper or scissors and then in comparison to the
    computer's choice, a winner will be declared through a string
    return.

--- script.js
+++ script.js
@@ -7,8 +7,48 @@ function getComputerChoice() {
     if (num < 1) {computerChoice = choices[0];}       
     else if (1 <= num & num < 2) {computerChoice = choices[1];}
     else {computerChoice = choices[2];}
-
-    console.log(computerChoice)
+    return computerChoice;
 }
 
-getComputerChoice()
+
+function declareWinner() {
+    let computerSelection = getComputerChoice().toLowerCase();
+    let playerSelection = prompt("Please enter your choice of either rock, paper, or scissors!").toLowerCase();
+    switch (computerSelection) {
+        case "rock":
+            if (playerSelection === computerSelection) {
+                return "It's a tie!";
+            } else if (playerSelection === "paper") {
+                return "You win! Paper beats rock!";
+            } else {
+                return "You lose! Rock beats scissors";
+            }
+            break;
+        case "paper":
+            if (playerSelection === computerSelection) {
+                return "It's a tie!";
+            } else if (playerSelection === "rock") {
+                return "You lose! Paper beats rock!";
+            } else {
+                return "You win! Scissors beat paper";
+            }
+            break;
+        case "scissors":
+            if (playerSelection === computerSelection) {
+                return "It's a tie!";
+            } else if (playerSelection === "rock") {
+                return "You win! Rock beats scissors!";
+            } else {
+                return "You lose! Scissors beat paper!";
+            }
+            break;
+    }
+
+}
+
+
+
+
+
+x = getComputerChoice()
+console.log(declareWinner())
