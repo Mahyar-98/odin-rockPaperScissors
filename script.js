d69412c0b7bcbb77b5bd6b04680674126b11a59b
Mahyar <mahyar.erfanian1998@gmail.com>
2023-01-26 15:19:35 -0500

    Add UI to the game with buttons instead of prompt

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
 choices = ['Rock', 'Paper', 'Scissors']
 
+const buttons = document.querySelectorAll("div button");
+const resultDiv = document.querySelector(".result");
+const playerScore = document.querySelector("div.player-score p");
+const computerScore = document.querySelector("div.computer-score p");
+const finalResult = document.querySelector(".final-result");
+
+
 function getComputerChoice() {
     let computerChoice = '';
     let num = Math.random()*3;
@@ -10,48 +17,73 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function playerSelect() {
+    buttons.forEach(button => {
+    button.addEventListener("click", e => playRound(e.target.textContent.toLowerCase()))
+    });
+}
 
-function playRound() {
+function playRound(playerSelection) {
     let computerSelection = getComputerChoice().toLowerCase();
-    let playerSelection = prompt("Please enter your choice of either rock, paper, or scissors!").toLowerCase();
-    switch (computerSelection) {
-        case "rock":
-            if (playerSelection === computerSelection) {
-                return "It's a tie!";
-            } else if (playerSelection === "paper") {
-                return "You win! Paper beats rock!";
-            } else {
-                return "You lose! Rock beats scissors";
-            }
-            break;
-        case "paper":
-            if (playerSelection === computerSelection) {
-                return "It's a tie!";
-            } else if (playerSelection === "rock") {
-                return "You lose! Paper beats rock!";
-            } else {
-                return "You win! Scissors beat paper";
-            }
-            break;
-        case "scissors":
-            if (playerSelection === computerSelection) {
-                return "It's a tie!";
-            } else if (playerSelection === "rock") {
-                return "You win! Rock beats scissors!";
-            } else {
-                return "You lose! Scissors beat paper!";
-            }
-            break;
+    let resultText = "";
+    if (+playerScore.textContent !== 5 && +computerScore.textContent !== 5) {
+        switch (computerSelection) {
+            case "rock":
+                if (playerSelection === computerSelection) {
+                    resultText = "It's a tie!";
+                } else if (playerSelection === "paper") {
+                    resultText = "You win! Paper beats rock!";
+                } else {
+                    resultText = "You lose! Rock beats scissors";
+                }
+                break;
+            case "paper":
+                if (playerSelection === computerSelection) {
+                    resultText = "It's a tie!";
+                } else if (playerSelection === "rock") {
+                    resultText = "You lose! Paper beats rock!";
+                } else {
+                    resultText = "You win! Scissors beat paper";
+                }
+                break;
+            case "scissors":
+                if (playerSelection === computerSelection) {
+                    resultText = "It's a tie!";
+                } else if (playerSelection === "rock") {
+                    resultText = "You win! Rock beats scissors!";
+                } else {
+                    resultText = "You lose! Scissors beat paper!";
+                }
+                break;
+        }
+        resultDiv.textContent += resultText + "\n";
 
+        if (resultText.includes("tie")) {
+            computerScore.textContent++;
+            playerScore.textContent++;
+        } else if (resultText.includes("win")) {
+            playerScore.textContent++;
+        } else {
+            computerScore.textContent++;
+        }
+    }
+    
+    if (+computerScore.textContent === 5) {
+        finalResult.textContent = "Computer is the winner!";
+    } else if (+playerScore.textContent === 5) {
+        finalResult.textContent = "Player is the winner";
+    } else if (+playerScore.textContent === 5 && +computerScore.textContent === 5){
+        finalResult.textContent = "It's a tie!";
     }
 
 }
 
-function game() {
+
+/*function game() {
     let computerScore = 0;
     let playerScore = 0;
     for (let i = 0; i < 5; i++) {
-        let result = playRound();
+        let result = playerSelect();
         console.log(result);
         if (result.includes("tie")) {
             computerScore++;
@@ -63,8 +95,7 @@ function game() {
         }
     }
     return `Player score is ${playerScore} and computer score is ${computerScore}.`
-}
+}*/
 
 
-
-console.log(game())
+    playerSelect()
